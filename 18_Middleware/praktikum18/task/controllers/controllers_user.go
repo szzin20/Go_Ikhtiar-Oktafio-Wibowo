51a25d0997856848929fc6091e0359b55291be66
szzin20 <92289030+szzin20@users.noreply.github.com>
2023-09-26 11:43:54 +0700

    submit tugas 18

---
+++ 18_Middleware/praktikum18/task/controllers/controllers_user.go
@@ -0,0 +1,152 @@
+package controllers
+
+import (
+	"middleware/middlewares"
+
+	"middleware/config"
+	"middleware/models"
+	"net/http"
+	"strconv"
+
+	"github.com/labstack/echo/v4"
+)
+
+// // get all users
+func GetUsersController(c echo.Context) error {
+	var users []models.User
+
+	if err := config.DB.Find(&users).Error; err != nil {
+		return echo.NewHTTPError(http.StatusBadRequest, err.Error())
+	}
+	return c.JSON(http.StatusOK, map[string]interface{}{
+		"message": "success get all users",
+		"users":   users,
+	})
+}
+
+// // get user by id
+func GetUserController(c echo.Context) error {
+
+	idParam := c.Param("id")
+	id, err := strconv.Atoi(idParam)
+	if err != nil {
+		return echo.NewHTTPError(http.StatusBadRequest, "Invalid ID")
+	}
+
+	var user models.User
+	if err := config.DB.First(&user, id).Error; err != nil {
+		return echo.NewHTTPError(http.StatusNotFound, "User not found")
+	}
+
+	return c.JSON(http.StatusOK, map[string]interface{}{
+		"message": "success get user",
+		"user":    user,
+	})
+}
+
+// create new user
+func CreateUserController(c echo.Context) error {
+	user := new(models.User)
+	if err := c.Bind(user); err != nil {
+		return echo.NewHTTPError(http.StatusBadRequest, err.Error())
+	}
+	if err := config.DB.Create(user).Error; err != nil {
+		return echo.NewHTTPError(http.StatusBadRequest, err.Error())
+	}
+
+	return c.JSON(http.StatusOK, map[string]interface{}{
+		"message": "success create new user",
+		"user":    user,
+	})
+}
+
+func LoginUserController(c echo.Context) error {
+	user := models.User{}
+	c.Bind(&user)
+
+	err := config.DB.Where("email = ? AND password = ?", user.Email, user.Password).First(&user).Error
+	if err != nil {
+		return c.JSON(http.StatusBadRequest, map[string]interface{}{
+			"message": "Fail login",
+			"error":   err.Error(),
+		})
+	}
+
+	userID := int(user.ID)
+
+	token, err := middlewares.CreateToken(userID, user.Name)
+	if err != nil {
+		return c.JSON(http.StatusBadRequest, map[string]interface{}{
+			"message": "Fail login",
+			"error":   err.Error(),
+		})
+	}
+
+	UsersResponse := models.UsersResponse{
+		UserID: user.ID,
+		Name:   user.Name,
+		Email:  user.Email,
+		Token:  token,
+	}
+
+	return c.JSON(http.StatusOK, map[string]interface{}{
+		"message": "success login",
+		"user":    UsersResponse,
+	})
+}
+
+// // delete user by id
+func DeleteUserController(c echo.Context) error {
+
+	idParam := c.Param("id")
+	id, err := strconv.Atoi(idParam)
+	if err != nil {
+		return echo.NewHTTPError(http.StatusBadRequest, "Invalid ID")
+	}
+
+	var user models.User
+	if err := config.DB.First(&user, id).Error; err != nil {
+		return echo.NewHTTPError(http.StatusNotFound, "User not found")
+	}
+
+	if err := config.DB.Delete(&user).Error; err != nil {
+		return echo.NewHTTPError(http.StatusBadRequest, err.Error())
+	}
+
+	return c.JSON(http.StatusOK, map[string]interface{}{
+		"message": "success delete user",
+	})
+}
+
+// // update user by id
+func UpdateUserController(c echo.Context) error {
+
+	idParam := c.Param("id")
+	id, err := strconv.Atoi(idParam)
+	if err != nil {
+		return echo.NewHTTPError(http.StatusBadRequest, "Invalid ID")
+	}
+
+	user := new(models.User)
+	if err := c.Bind(user); err != nil {
+		return echo.NewHTTPError(http.StatusBadRequest, err.Error())
+	}
+
+	var existingUser models.User
+	if err := config.DB.First(&existingUser, id).Error; err != nil {
+		return echo.NewHTTPError(http.StatusNotFound, "User not found")
+	}
+
+	existingUser.Name = user.Name
+	existingUser.Email = user.Email
+	existingUser.Password = user.Password
+
+	if err := config.DB.Save(&existingUser).Error; err != nil {
+		return echo.NewHTTPError(http.StatusBadRequest, err.Error())
+	}
+
+	return c.JSON(http.StatusOK, map[string]interface{}{
+		"message": "success update user",
+		"user":    existingUser,
+	})
+}
---
+++ 18_Middleware/praktikum18/task/middlewares/middlewares_jwt.go
@@ -0,0 +1,28 @@
+package middlewares
+
+import (
+	"middleware/constants"
+
+	"time"
+
+	"github.com/golang-jwt/jwt"
+	echojwt "github.com/labstack/echo-jwt/v4"
+	"github.com/labstack/echo/v4"
+)
+
+func CreateToken(userId int, name string) (string, error) {
+	claims := jwt.MapClaims{}
+	claims["userId"] = userId
+	claims["name"] = name
+	claims["exp"] = time.Now().Add(time.Hour * 1).Unix()
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	return token.SignedString([]byte(constants.SECRET_JWT))
+}
+
+func JWTMiddleware() echo.MiddlewareFunc {
+	return echojwt.WithConfig(echojwt.Config{
+		SigningKey:    []byte(constants.SECRET_JWT),
+		SigningMethod: "HS256",
+	})
+}
