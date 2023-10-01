e75d062801af4cbddf6814773debe867070af4b8
szzin20 <92289030+szzin20@users.noreply.github.com>
2023-10-01 12:03:33 +0700

    Update testcalculate_test.go

--- 19_Unit_Testing/praktikum19/simple_testing/testcalculate_test.go
+++ 19_Unit_Testing/praktikum19/simple_testing/testcalculate_test.go
@@ -7,37 +7,37 @@ import (
 func TestCalculator(t *testing.T) {
 	c := calculator{}
 
-	// pengetesan untuk penjumlahan
+	// addition test
 	result := c.Addition(5, 3)
 	if result != 8 {
 		t.Errorf("Hasilnya yaitu: 8, Hasil Run :  %f", result)
 	}
 
-	// pengetesan untuk pengurangan
+	// substraction test
 	result = c.Subtraction(5, 3)
 	if result != 2 {
 		t.Errorf("Hasilnya yaitu: 2, Hasil Run :  %f", result)
 	}
 
-	// pengetesan untuk pembagian
+	// division test
 	result, err := c.Division(6, 2)
 	if err != nil || result != 3 {
 		t.Errorf("Hasilnya yaitu: 3 tanpa error, tetapi hasilnya %f dengan error: %v", result, err)
 	}
 
-	// pengetesan untuk pembagian oleh 0 (error case)
+	// division test by o
 	_, err = c.Division(6, 0)
 	if err == nil {
 		t.Errorf("Hasilnya yaitu ada error saat membagi oleh 0, tetapi tidak ada error yang ditemukan")
 	}
 
-	// pengetesan untuk perkalian
+	// multiplication test
 	result = c.Multiplication(5, 3)
 	if result != 15 {
 		t.Errorf("Hasilnya yaitu: 15, Hasil Run :  %f", result)
 	}
 
-	// Tambahkan pengetesan untuk mencakup jalur alternatif dalam Division
+	// add test alternative division
 	result, err = c.Division(3, 0)
 	if err == nil {
 		t.Errorf("Hasilnya yaitu ada error saat membagi oleh 0, tetapi tidak ada error yang ditemukan")
