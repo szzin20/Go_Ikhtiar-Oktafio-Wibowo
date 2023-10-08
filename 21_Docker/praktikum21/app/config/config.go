4a7cd5e93bd6592118ab640151afe153b33cb888
szzin20 <92289030+szzin20@users.noreply.github.com>
2023-10-08 22:31:08 +0700

    update

--- 21_Docker/praktikum21/app/config/config.go
+++ 21_Docker/praktikum21/app/config/config.go
@@ -3,9 +3,9 @@ package config
 import (
 	"os"
 	"strconv"
-
-	"github.com/joho/godotenv"
 	"github.com/sirupsen/logrus"
+	"github.com/joho/godotenv"
+	
 )
 
 type AppConfig struct {
@@ -18,61 +18,61 @@ type AppConfig struct {
 }
 
 func InitConfig() *AppConfig {
-	var res = new(AppConfig)
-	res = loadConfig()
+    var res = new(AppConfig)
+    res = loadConfig()
 
-	if res == nil {
-		logrus.Fatal("Config : Cannot start program, failed to load configuration")
-		return nil
-	}
+    if res == nil {
+        logrus.Fatal("Config : Cannot start program, failed to load configuration")
+        return nil
+    }
 
-	return res
+    return res
 }
 
+
 func loadConfig() *AppConfig {
-	var res = new(AppConfig)
+    var res = new(AppConfig)
 
-	err := godotenv.Load(".env.db")
+    err := godotenv.Load(".env")
 
-	if err != nil {
-		logrus.Error("Config : Cannot load config file,", err.Error())
-		return nil
-	}
+    if err != nil {
+        logrus.Error("Config : Cannot load config file,", err.Error())
+    }
 
-	if val, found := os.LookupEnv("SERVERPORT"); found {
-		port, err := strconv.Atoi(val)
-		if err != nil {
-			logrus.Error("Config : invalid port value,", err.Error())
-			return nil
-		}
-		res.SERVERPORT = port
-	}
+    if val, found := os.LookupEnv("SERVERPORT"); found {
+        port, err := strconv.Atoi(val)
+        if err != nil {
+            logrus.Error("Config : invalid port value,", err.Error())
+            return nil
+        }
+        res.SERVERPORT = port
+    }
 
-	if val, found := os.LookupEnv("DBPORT"); found {
-		port, err := strconv.Atoi(val)
-		if err != nil {
-			logrus.Error("Config : invalid db port value,", err.Error())
-			return nil
-		}
-		res.DBPORT = port
-	}
+    if val, found := os.LookupEnv("DBPORT"); found {
+        port, err := strconv.Atoi(val)
+        if err != nil {
+            logrus.Error("Config : invalid db port value,", err.Error())
+            return nil
+        }
+        res.DBPORT = port
+    }
 
-	if val, found := os.LookupEnv("DBHOST"); found {
-		res.DBHOST = val
-	}
+    if val, found := os.LookupEnv("DBHOST"); found {
+        res.DBHOST = val
+    }
 
-	if val, found := os.LookupEnv("DBUSER"); found {
-		res.DBUSER = val
-	}
+    if val, found := os.LookupEnv("DBUSER"); found {
+        res.DBUSER = val
+    }
 
-	if val, found := os.LookupEnv("DBPASS"); found {
-		res.DBPASS = val
-	}
+    if val, found := os.LookupEnv("DBPASS"); found {
+        res.DBPASS = val
+    }
 
-	if val, found := os.LookupEnv("DBNAME"); found {
-		res.DBNAME = val
-	}
+    if val, found := os.LookupEnv("DBNAME"); found {
+        res.DBNAME = val
+    }
 
-	return res
+    return res
 
 }
